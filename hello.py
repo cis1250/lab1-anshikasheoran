fcb46dec7527e49757ac3aa1e85db13e19edac18
Anshika <asheoran@uoguelph.ca>
2025-09-17 11:39:41 -0400

    hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-
-# This is your first Python program!
-# TODO:
-# 1. Keep "Hello, world!" as the first line.
-# 2. Add another print statement with your name.
-# 3. Add another print statement with your favorite hobby.
-
 print("Hello, world!")
+print("I am Anshika")
+print("My favourite hobby is listening to music because it is very relaxing.")
